84c1e510b331266f7cb54bee800e50d0b74cc99e
makkov <maxim_86nva@mail.ru>
2024-07-31 10:20:41 +0300

    init project

---
+++ src/main/java/com/colvir/colvirk8s/ColvirK8sApplication.java
@@ -0,0 +1,13 @@
+package com.colvir.colvirk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColvirK8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColvirK8sApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/colvir/colvirk8s/ColvirK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colvir.colvirk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColvirK8sApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
