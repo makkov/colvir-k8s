eea9b32087c03edb43e9400a0e32782185cc3767
makkov <maxim_86nva@mail.ru>
2024-08-05 19:28:23 +0300

    spring profiles

--- src/main/java/com/colvir/colvirk8s/HelloController.java
+++ src/main/java/com/colvir/colvirk8s/HelloController.java
@@ -2,6 +2,7 @@ package com.colvir.colvirk8s;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +11,14 @@ import java.time.LocalDateTime;
 @RestController
 public class HelloController {
 
+    @Value("${hello.text}")
+    private String helloText;
+
     private final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping(("/hello"))
     public String sayHello() {
         logger.info("Requested at {}", LocalDateTime.now());
-        return "hello";
+        return helloText;
     }
 }
